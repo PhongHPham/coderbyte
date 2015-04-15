e417abfa61ac06dec0ebf910b115e757a87ad5a3
Phong Pham <phizzy@gmail.com>
2015-04-15 12:34:20 +0700

    Create firstReverse.js

---
+++ firstReverse.js
@@ -0,0 +1,7 @@
+// function firstReverse takes a string parameter and returns the string in reversed order
+
+function firstReverse(str) { 
+	return str.split('').reverse().join('');
+
+         
+}
